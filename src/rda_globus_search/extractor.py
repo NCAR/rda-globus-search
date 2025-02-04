c34120f04da6bbfc55525c4a998474aca36eb458
Thomas Cram <tcram@ucar.edu>
2025-02-04 15:58:58 -0700

    added db queries

--- src/rda_globus_search/extractor.py
+++ src/rda_globus_search/extractor.py
@@ -6,20 +6,61 @@ import click
 
 from .lib import common_options, prettyprint_json
 from .lib.database import load_search_db
-from rda_python_common.PgDBI import pgget
+from rda_python_common.PgDBI import pgget, pgmget
 
 def get_search_metadata(dataset):
     """ Query and return search metadata """
+
     search_db = load_search_db()
     cond = "dsid='{}'".format(dataset)
     myrec = pgget('datasets', 'title, summary', cond)
     title = myrec['title']
     description = myrec['summary']
-    data_types = pgget('data_types', 'DISTINCT(keyword) as data_type', cond)
+    data_types = pgmget('data_types', 'DISTINCT(keyword) as data_type', cond)
 
+    keyword_query = "SELECT " \
+        "CONCAT('EARTH SCIENCE > ', topic, ' > ', term, ' > ', keyword) " \
+        "AS keywords " \
+        "FROM gcmd_variables WHERE dsid='{}'".format(dataset)
+    gcmd_keywords = pgmget(None, None, keyword_query)
+
+    time_resolutions = pgmget('time_resolutions', 'DISTINCT(keyword) as time_resolutions', cond)
+    if not time_resolutions:
+        time_resolution = {'time resolution': None}
+    else:
+        time_resolution = {'time resolution': time_resolutions['time_resolutions']}
+
+    platform_query = "SELECT path " \
+        "FROM search.platforms_new AS p " \
+        "LEFT JOIN search.gcmd_platforms AS g " \
+        "ON g.uuid = p.keyword " \
+        "WHERE p.dsid = '{}'".format(dataset)
+    platforms = pgmget(None, None, platform_query)
+
+    grid_resolutions = pgmget('grid_resolutions', 'keyword', cond)
+    if not grid_resolutions:
+        grid_resolution = {'spatial resolution': None}
+    else:
+        grid_resolution = {'spatial resolution': grid_resolutions['keyword']}
+    
+    topics = pgmget('topics', 'keyword', cond)
+
+    project_query = "SELECT path " \
+        "FROM search.projects_new AS p " \
+        "LEFT JOIN search.gcmd_projects AS g " \
+        "ON g.uuid = p.keyword " \
+        "WHERE p.dsid = '{}'".format(dataset)
+    projects = pgmget(None, None, project_query)
+
+    # load query results into common dict
     search_metadata = {}
     search_metadata.update({'title': title, 'description': description})
-    search_metadata.update(data_types)
+    search_metadata.update({'data type': data_types['data_type']})
+    search_metadata.update({'GCMD keywords': gcmd_keywords['keywords']})
+    search_metadata.update(time_resolution)
+    search_metadata.update({'platform': platforms['path']})
+    search_metadata.update(grid_resolution)
+    search_metadata.update({'topic': topics['topics']})
 
     return search_metadata
 
