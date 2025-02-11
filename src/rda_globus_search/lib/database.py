552fffb0b89abed34612a7ff6477eb6d9b03e30a
Thomas Cram <tcram@ucar.edu>
2025-02-11 10:50:20 -0700

    add db configs

--- src/rda_globus_search/lib/database.py
+++ src/rda_globus_search/lib/database.py
@@ -1,14 +1,30 @@
 import yaml
 from rda_python_common.PgDBI import default_scinfo
 
+DATABASE_CONFIG = '/glade/u/home/rdadata/.pgconfig.yml'
+
 def get_dbconfigs():
     """ Get DB login config for all databases """
 
-    with open('/glade/u/home/rdadata/.pgconfig.yml') as f:
+    with open(DATABASE_CONFIG) as f:
         try:
             return yaml.safe_load(f)
         except yaml.YAMLError as e:
             print(e)
+
+def load_db(database, schema=None):
+    """ 
+    Set the database connection to the given DB/schema.
+    'database' can be one of 'dssdb', 'search', or 'wagtail'
+    """
+    
+    dbconfigs = get_dbconfigs()
+    config_name = '{}_config'.format(database)
+    dbconfig = dbconfigs[config_name]
+    if not schema:
+        pgschema = dbconfigs['pg_schemas'][database]
+
+    return default_scinfo(dbconfig['dbname'], pgschema, dbconfig['host'], dbconfig['user'])
 
 def load_search_db():
     """ Set the database connection to the search DB """
@@ -18,3 +34,21 @@ def load_search_db():
     pgschemas = dbconfigs['pg_schemas']
 
     return default_scinfo(search_dbconfig['dbname'], pgschemas['search'], search_dbconfig['host'], search_dbconfig['user'])
+
+def load_dssdb_db():
+    """ Set the database connection to dssdb """
+    
+    dbconfigs = get_dbconfigs()
+    dssdb_dbconfig = dbconfigs['dssdb_config']
+    pgschemas = dbconfigs['pg_schemas']
+
+    return default_scinfo(dssdb_dbconfig['dbname'], pgschemas['dssdb'], dssdb_dbconfig['host'], dssdb_dbconfig['user'])
+
+def load_wagtail_db():
+    """ Set the database connection to wagtail """
+    
+    dbconfigs = get_dbconfigs()
+    wagtail_dbconfig = dbconfigs['wagtail_config']
+    pgschemas = dbconfigs['pg_schemas']
+
+    return default_scinfo(wagtail_dbconfig['dbname'], pgschemas['wagtail'], wagtail_dbconfig['host'], wagtail_dbconfig['user'])
