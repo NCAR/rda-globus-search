f86f7ecbf91a4955382d18075c5f85b51febe72e
Thomas Cram <tcram@ucar.edu>
2025-02-13 17:18:49 -0700

    updated auth client

--- src/rda_globus_search/lib/auth.py
+++ src/rda_globus_search/lib/auth.py
@@ -25,21 +25,6 @@ def token_storage_adapter():
         token_storage_adapter._instance = SimpleJSONFileAdapter(TOKEN_FILE)
     return token_storage_adapter._instance
 
-
 def auth_client():
-    storage_adapter = token_storage_adapter()
-
-    authdata = storage_adapter.get_token_data(AUTH_RESOURCE_SERVER)
-    access_token = authdata["access_token"]
-    refresh_token = authdata["refresh_token"]
-    access_token_expires = authdata["expires_at_seconds"]
-
-    authorizer = globus_sdk.RefreshTokenAuthorizer(
-        refresh_token,
-        internal_auth_client(),
-        access_token,
-        int(access_token_expires),
-        on_refresh=storage_adapter.on_refresh,
-    )
-
+    authorizer = globus_sdk.ClientCredentialsAuthorizer(internal_auth_client, APP_SCOPES)
     return globus_sdk.AuthClient(authorizer=authorizer, app_name="dataset-search")
