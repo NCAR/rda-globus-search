7b65a16542a023f482d5f0c8fd8e8977f50c98bf
Thomas Cram <tcram@ucar.edu>
2025-02-13 17:06:32 -0700

    set up confidential client auth

--- src/rda_globus_search/lib/auth.py
+++ src/rda_globus_search/lib/auth.py
@@ -1,17 +1,23 @@
-import os
-
-import click
 import globus_sdk
 from globus_sdk.tokenstorage import SimpleJSONFileAdapter
 
 AUTH_RESOURCE_SERVER = "auth.globus.org"
 
-CLIENT_ID = "48ea646d-9628-4363-baa7-d22701b23233"
-TOKEN_FILE = "/glade/u/home/rdadata/lib/python/globus_rda_search_tokens.json"
+CLIENT_CONFIG = '/glade/u/home/rdadata/.globusconfig.yml'
+
+def get_client_credentials():
+    """ Get Globus Search service client ID and secret """
+    with open(CLIENT_CONFIG) as f:
+        try:
+            return yaml.safe_load(f)
+        except yaml.YAMLError as e:
+            print(e)
 
 def internal_auth_client():
-    return globus_sdk.NativeAppAuthClient(CLIENT_ID, app_name="dataset-search")
-
+    client_config = get_client_credentials['search_client']
+    client_id = client_config['client_id']
+    client_secret = client_config['client_secret']
+    return globus_sdk.ConfidentialAppAuthClient(client_id, client_secret, app_name="dataset-search")
 
 def token_storage_adapter():
     if not hasattr(token_storage_adapter, "_instance"):
--- src/rda_globus_search/lib/search.py
+++ src/rda_globus_search/lib/search.py
@@ -1,25 +1,10 @@
 import globus_sdk
 
-from .auth import internal_auth_client, token_storage_adapter
+from .auth import internal_auth_client
+from .lib import APP_SCOPES
 
 SEARCH_RESOURCE_SERVER = "search.api.globus.org"
 
-
-def search_client(authenticated=True):
-    storage_adapter = token_storage_adapter()
-
-    authorizer = None
-    if authenticated:
-        authdata = storage_adapter.get_token_data(SEARCH_RESOURCE_SERVER)
-        access_token = authdata["access_token"]
-        refresh_token = authdata["refresh_token"]
-        access_token_expires = authdata["expires_at_seconds"]
-        authorizer = globus_sdk.RefreshTokenAuthorizer(
-            refresh_token,
-            internal_auth_client(),
-            access_token,
-            int(access_token_expires),
-            on_refresh=storage_adapter.on_refresh,
-        )
-
+def search_client():
+    authorizer = globus_sdk.ClientCredentialsAuthorizer(internal_auth_client, APP_SCOPES)
     return globus_sdk.SearchClient(authorizer=authorizer, app_name="dataset-search")
