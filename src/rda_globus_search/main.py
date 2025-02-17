65a408c51e4adecb2dad4b4fca0c4e8f2bf7abce
Thomas Cram <tcram@ucar.edu>
2025-02-17 12:08:16 -0700

    uncommented assembler and watcher

--- src/rda_globus_search/main.py
+++ src/rda_globus_search/main.py
@@ -1,7 +1,7 @@
 import click
 
-# from . import assembler, extractor, manage_index, query, submitter, watcher
-from . import extractor, assembler, manage_index
+from . import extractor, assembler, submitter, watcher, manage_index
+#from . import query
 from .lib import common_options
 
 @click.group("dataset-search")
@@ -9,15 +9,14 @@ from .lib import common_options
 def cli():
     pass
 
-
 # index management
 manage_index.add_commands(cli)
 
 # cli workflow
 cli.add_command(extractor.extract_cli)
 cli.add_command(assembler.assemble_cli)
-#cli.add_command(submitter.submit_cli)
-#cli.add_command(watcher.watch_cli)
+cli.add_command(submitter.submit_cli)
+cli.add_command(watcher.watch_cli)
 
 # query results
 #cli.add_command(query.query_cli)
