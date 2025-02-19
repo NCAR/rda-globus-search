0e0cbaaa53fd76110a2c0d328419d8f5ae6c97d9
Thomas Cram <tcram@ucar.edu>
2025-02-19 16:10:12 -0700

    fix bug in dump_query

--- src/rda_globus_search/lib/__init__.py
+++ src/rda_globus_search/lib/__init__.py
@@ -45,8 +45,8 @@ def all_filenames(directory, pattern=None):
 
 def prettyprint_json(obj, fp=None):
     if fp:
-        return json.dump(obj, fp, indent=2, separators=(",", ": "), ensure_ascii=False)
-    return json.dumps(obj, indent=2, separators=(",", ": "), ensure_ascii=False)
+        return json.dump(obj.__dict__, fp, indent=2, separators=(",", ": "), ensure_ascii=False)
+    return json.dumps(obj.__dict__, indent=2, separators=(",", ": "), ensure_ascii=False)
 
 __all__ = (
     "EXTRACTED_OUTPUT",
