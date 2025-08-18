2d5abe42c45667ede8e008f6ea1e312d33b3c781
Thomas Cram <tcram@ucar.edu>
2025-08-17 22:37:52 -0600

    add ingest subcommand

---
+++ src/rda_globus_search/ingester.py
@@ -0,0 +1,18 @@
+import click
+import logging
+from . import extractor, assembler, submitter, watcher
+
+@click.command(
+    help="Run all workflow commands in sequence: extract, assemble, submit, watch.  This is equivalent to running each subcommand (extract, assemble, submit, watch) individually."
+)
+@click.option(
+    "--dsid",
+    help="Dataset ID (dnnnnnn) to process."
+)
+@click.pass_context
+def ingest(ctx, dsid):
+    ctx = click.get_current_context()
+    ctx.invoke(extractor.extract, dsid=dsid)
+    ctx.invoke(assembler.assemble)
+    ctx.invoke(submitter.submit)
+    ctx.invoke(watcher.watch)
--- src/rda_globus_search/main.py
+++ src/rda_globus_search/main.py
@@ -6,7 +6,8 @@ from . import (
     extractor, 
     assembler, 
     submitter, 
-    watcher, 
+    watcher,
+    ingester, 
     manage_index, 
     manage_subject,
     query
@@ -25,13 +26,14 @@ def cli():
 manage_index.add_commands(cli)
 
 # cli workflow
-cli.add_command(extractor.extract_cli)
-cli.add_command(assembler.assemble_cli)
-cli.add_command(submitter.submit_cli)
-cli.add_command(watcher.watch_cli)
+cli.add_command(extractor.extract)
+cli.add_command(assembler.assemble)
+cli.add_command(submitter.submit)
+cli.add_command(watcher.watch)
+cli.add_command(ingester.ingest)
 
 # query results
-cli.add_command(query.query_cli)
+cli.add_command(query.query)
 
 # subject management
 manage_subject.add_commands(cli)
